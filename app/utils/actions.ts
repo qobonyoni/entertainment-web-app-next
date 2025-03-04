f711e01849c1bd8732eee6810240dd6aa91f5a0b
Qobo <quenyoni@gmail.com>
2025-03-04 13:33:38 +0200

    updated actions2

--- app/utils/actions.ts
+++ app/utils/actions.ts
@@ -1,5 +1,5 @@
 'use server'
-const apiKey =process.env.NEXT_PUBLIC_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
     
 export async function getPopularMovies() {
     try {
@@ -13,12 +13,12 @@ export async function getPopularMovies() {
         }
   
         const res = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US`, options)
-        const data = res.json();
+        const data =await res.json();
         return data
 
         
     }
-    catch {
-        console.error("Something wrong happened")
+    catch(error) {
+        console.error("Something wrong happened"+ error.message)
     }
 }
